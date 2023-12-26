59d5bec9d114328c18f6fc68791de14a21b7a46e
Mrfroxi <froxi340@mail.ru>
2023-12-26 15:50:53 +0300

    feat:added babel plugin and i18n-extracted plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,27 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
       }
 
+    const babelLoader ={
+              test: /\.(js|jsx|tsx)$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ['@babel/preset-env'],
+                  "plugins": [
+                    [
+                        "i18next-extract", 
+                        {
+                            locales: ['ru' , 'en'],
+                            keyAsDefaultValue:true,
+                        }
+                    ],
+               
+                  ]
+                }
+              }
+    }
+
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif)$/i,
@@ -48,6 +69,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     return [
         svgLoader,
+        babelLoader,
         fileLoader,
         typescriptLoader,
         cssLoader,
